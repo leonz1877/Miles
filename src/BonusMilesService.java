6a21b7151f74f07540db134d154535a8ad7a116d
Leonid Zhdanov <zhdanov.leon@gmail.com>
2022-05-21 20:56:59 +0400

    created metod BonusMilesService

---
+++ src/BonusMilesService.java
@@ -0,0 +1,11 @@
+public class BonusMilesService {
+
+    public int calculate (int price) {
+
+        int result = price / 20;
+
+        return result;
+
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int ticketPrice = 7500; // объявляем переменную ticketPrice и помещаем стоимость билета
+        BonusMilesService service = new BonusMilesService();
 
-        int bonusMiles = ticketPrice / 20; // объявляем переменную bonusMiles, считаем бонусы и помещаем значение
+        int price = 10_000;
 
-        System.out.print("Ваши бонусы: "); // Вывод в консоль текстового сообщения
+        int miles = service.calculate(price);
 
-        System.out.println(bonusMiles); // Вывод в консоль количества бонусов
+        System.out.println(miles);
     }
 }
